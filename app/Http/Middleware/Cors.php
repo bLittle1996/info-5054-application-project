e4198dd5cec8b1dabb40d7def9c85886b5a79693
Ben Littleton <B.Little1996@gmail.com>
2017-03-02 14:28:44 -0500

    Added additional headers so remote Auth works

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,6 +18,8 @@ class Cors
     {
         $response = $next($request);
         $response->headers->set("Access-Control-Allow-Origin", "*");
+        $response->headers->set("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+        $response->headers->set("Access-Control-Allow-Headers", "Content-Type,Authorization,X-Requested-With");
 
         return $response;
     }
